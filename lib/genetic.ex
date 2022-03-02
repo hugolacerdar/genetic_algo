309b9144401e629ae8b3394cefd2db53cbe1a96a
Hugo <hugolacerdar@gmail.com>
2022-03-02 16:11:44 -0300

    fix: solve warning problems

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -22,26 +22,26 @@ defmodule Genetic do
 
   def initialize(genotype, opts \\ []) do
     population_size = Keyword.get(opts, :population_size, 100)
-    for _ <- 1..100, do: genotype.()
+    for _ <- 1..population_size, do: genotype.()
   end
 
-  def evaluate(population, fitness_function, opts \\ []) do
-    population 
+  def evaluate(population, fitness_function, _opts \\ []) do
+    population
     |> Enum.sort_by(fitness_function, &>=/2)
   end
 
-  def select(population, opts \\ []) do
+  def select(population, _opts \\ []) do
     population
     |> Enum.chunk_every(2)
     |> Enum.map(&List.to_tuple(&1))
   end
 
-  def crossover(population, opts \\ []) do
-    population 
+  def crossover(population, _opts \\ []) do
+    population
     |> Enum.reduce([],
-      fn {p1, p2}, acc -> 
+      fn {p1, p2}, acc ->
         cx_point = :rand.uniform(length(p1))
-        {{h1, t1}, {h2, t2}} = 
+        {{h1, t1}, {h2, t2}} =
           {Enum.split(p1, cx_point),
           Enum.split(p2, cx_point)}
         {c1, c2} = {h1 ++ t2, h2 ++ t1}
@@ -49,8 +49,8 @@ defmodule Genetic do
       end)
   end
 
-  def mutation(population, opts \\ []) do
-    population 
+  def mutation(population, _opts \\ []) do
+    population
     |> Enum.map(fn chromosome ->
       if :rand.uniform < 0.05 do
         Enum.shuffle(chromosome)
