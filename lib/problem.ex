402c1177c270e0406a779df4c0dab1e583c959ba
Hugo <hugolacerdar@gmail.com>
2022-03-07 14:42:51 -0300

    chore: add generation number to terminate? callback

--- lib/problem.ex
+++ lib/problem.ex
@@ -1,10 +1,10 @@
 defmodule Problem do
   alias Types.Chromosome
 
-  @callback genotype :: Chromosome.t
+  @callback genotype :: Chromosome.t()
 
-  @callback fitness_function(Chromosome.t) :: number()
+  @callback fitness_function(Chromosome.t()) :: number()
 
-  @callback terminate?(Enum.t) :: boolean()
+  @callback terminate?(Enum.t(), integer()) :: boolean()
 
 end
