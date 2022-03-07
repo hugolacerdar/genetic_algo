cc0d62a509fc58d8c6d658474365173af00789d6
Hugo <hugolacerdar@gmail.com>
2022-03-07 14:43:46 -0300

    chore: set terminate? callback to return true on 100th generation

--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -5,17 +5,17 @@ defmodule OneMax do
   @impl true
   def genotype do
     genes = for _ <- 1..42, do: Enum.random(0..1)
-    %Chromosome{genes: genes}
+    %Chromosome{genes: genes, size: 42}
   end
 
   @impl true
   def fitness_function(chromosome), do: Enum.sum(chromosome.genes)
 
   @impl true
-  def terminate?([best | _]), do: best.fitness == 42
+  def terminate?([best | _], generation), do: generation == 100
 end
 
-soln = Genetic.run(OneMax)
+soln = Genetic.run(OneMax, population_size: 10)
 
 IO.write("\n")
 IO.inspect(soln)
