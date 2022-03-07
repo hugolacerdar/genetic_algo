2cb411ce385d12a61f9a6c9e4ac07a838f687d8c
Hugo <hugolacerdar@gmail.com>
2022-03-07 13:57:21 -0300

    chore: add penalty if limit is exceeded

--- scripts/cargo.exs
+++ scripts/cargo.exs
@@ -11,10 +11,24 @@ defmodule Cargo do
   @impl true
   def fitness_function(chromosome) do
     profits = [6, 5, 8, 9, 6, 7, 3, 1, 2, 6]
-    profits
-    |> Enum.zip(chromosome.genes)
-    |> Enum.map(fn {p,g} -> p * g end)
-    |> Enum.sum()
+    weights = [10, 6, 8, 7, 10, 9, 7, 11, 6, 8]
+    weight_limit = 40
+
+    potential_profits =
+      chromosome.genes
+      |> Enum.zip(profits)
+      |> Enum.map(fn {p,g} -> p * g end)
+      |> Enum.sum()
+
+      over_limit? =
+        chromosome.genes
+        |> Enum.zip(weights)
+        |> Enum.map(fn {c, w} -> c * w end)
+        |> Enum.sum()
+        |> Kernel.>(weight_limit)
+
+      profits = if over_limit?, do: 0, else: potential_profits
+      profits
   end
 
   @impl true
